be568e1bd268e06c93afc746baacd5da1794d238
chemesh <roy12sh@gmail.com>
2022-04-19 16:47:40 +0300

    added doc to logger module

--- utils/Logger.py
+++ utils/Logger.py
@@ -7,11 +7,22 @@ LOG_FILENAME_FORMAT = "%Y/%m/%d,%H"
 
 
 class Logger(object):
+    """
+    Logger class for logging.
+    use example:
+        logger = Logger()
+        logger.info("this is a log")
+    """
     def __init__(self):
         self._logs_folder = Path(LOG_FOLDER_PATH)
         self._logs_folder.mkdir(parents=True, exist_ok=True)
 
-    def _record(self, msg, timestamp):
+    def _record(self, msg: str, timestamp: datetime) -> None:
+        """
+        Record the log in a log file, determined by the timestamp
+        :param msg: Log message
+        :param timestamp:  datetime object
+        """
         file = self._get_path(timestamp)
         with file.open(mode="a") as f:
             f.write(msg+"\n")
@@ -30,31 +41,55 @@ class Logger(object):
         path.touch(exist_ok=True)
         return path
 
-    def _log(self, log_type, msg, record=True, output=sys.stdout):
+    def _log(self, log_type: str, msg: str, record=True, output=sys.stdout) -> None:
+        """
+        Writes log to output
+        :param log_type: string identifier for log type (i.e. "INFO")
+        :param msg: log message
+        :param record: if true, record log in file
+        :param output: where to write the log to (default is stdout)
+        """
         time = datetime.now()
         log = f"[{time}] __{log_type}__: {msg}"
         print(log, file=output)
         if record:
             self._record(log, time)
 
-    def debug(self, msg, record=True):
+    def debug(self, msg: str, record=True):
+        """
+        Write debug log.
+        :param msg: log message
+        :param record: if true, record log in file
+        """
         self._log(
             log_type="DEBUG",
             msg=msg,
             record=record)
 
-    def info(self, msg):
+    def info(self, msg: str):
+        """
+        Write informative log
+        :param msg: log message
+        """
         self._log(
             log_type="INFO",
             msg=msg)
 
-    def warn(self, msg):
+    def warn(self, msg: str):
+        """
+        Write warning log
+        :param msg: log message
+        """
         self._log(
             log_type="WARNING",
             msg=msg,
             output=sys.stderr)
 
-    def error(self, msg):
+    def error(self, msg: str):
+        """
+        Write error log
+        :param msg: log message
+        """
         self._log(
             log_type="ERROR",
             msg=msg,
