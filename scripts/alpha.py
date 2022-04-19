81ae5789c1ac39ed46bd9c167c1b5916c3308d51
chemesh <roy12sh@gmail.com>
2022-04-19 15:59:15 +0300

    added logger + data models

--- scripts/alpha.py
+++ scripts/alpha.py
@@ -2,8 +2,7 @@ from deap import base, creator, tools
 import mingus
 import librosa
 import matplotlib.pyplot as plt
-import numpy as np
-from librosa import display
+
 
 
 
@@ -14,34 +13,15 @@ def main():
     data, samplerate = librosa.load(WAV_FILE_TEST)
     print(f"data: {data}, sample rate: {samplerate}")
 
-    tempo, beat_frames = librosa.beat.beat_track(y=data, sr=samplerate)
+    hop_length = 512
+    h_data, p_data = librosa.effects.hpss(data)
+    print(f"decomposition values - \nHarmonic data: {h_data}\n Percussive data: {p_data}")
+
+    tempo, beat_frames = librosa.beat.beat_track(y=p_data, sr=samplerate)
     print(f"tempo: {tempo:.2f}")
 
     beat_times = librosa.frames_to_time(beat_frames, sr=samplerate)
     print(f"beat times: {beat_times}")
-
-    d = librosa.stft(data)
-    h, p = librosa.decompose.hpss(d)
-    print(f"decomposition values - \nH: {h}\n P: {p}")
-
-    fig, ax = plt.subplots(nrows=3, sharex=True, sharey=True)
-    img = librosa.display.specshow(librosa.amplitude_to_db(np.abs(d),
-                                                           ref=np.max),
-                                   y_axis='log', x_axis='time', ax=ax[0])
-    ax[0].set(title='Full power spectrogram')
-    ax[0].label_outer()
-    librosa.display.specshow(librosa.amplitude_to_db(np.abs(h),
-                                                     ref=np.max(np.abs(d))),
-                             y_axis='log', x_axis='time', ax=ax[1])
-    ax[1].set(title='Harmonic power spectrogram')
-    ax[1].label_outer()
-    librosa.display.specshow(librosa.amplitude_to_db(np.abs(p),
-                                                     ref=np.max(np.abs(p))),
-                             y_axis='log', x_axis='time', ax=ax[2])
-    ax[2].set(title='Percussive power spectrogram')
-    fig.colorbar(img, ax=ax, format='%+2.0f dB')
-    fig.show()
-
 
 
 if __name__ == "__main__":
