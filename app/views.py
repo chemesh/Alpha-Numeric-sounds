e6ad67d9666f6a9c81b4b622a6b33bca4ec94535
chemesh <roy12sh@gmail.com>
2022-09-02 16:46:26 +0300

    try 1

--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.models import UserManager
+from secrets import token_urlsafe
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
+N_BYTES = 32
 
-def index(request):
+manager = UserManager()
+
+def index(request: HttpRequest):
+
     return HttpResponse("Hello, world. You're at the Alpha Numeric Sounds index.")
+
+def get_token(request):
+    token = token_urlsafe(N_BYTES)
+    manager.create_user()
+
+
+def add_songs_from_url(request):
