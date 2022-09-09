7aa847603e4757b563fc29492037e74e39c5a43d
chemesh <roy12sh@gmail.com>
2022-09-09 14:55:23 +0300

    import spleeter from pypi

--- startup.bat
+++ startup.bat
@@ -1,6 +1,5 @@
 pip install --upgrade pip
 pip install -r requirements.txt
-pip install dependencies\spleeter-2.3.2-py3-none-any.whl
 
 set PYTHONPATH=%PYTHONPATH%;cd
 
--- startup.sh
+++ startup.sh
@@ -1,6 +1,5 @@
 
 pip install --upgrade pip
 pip install -r ./requirements.txt
-pip install ./dependencies/spleeter-2.3.2-py3-none-any.whl
 python ./server/manage.py migrate
 python ./server/manage.py runserver 0.0.0.0:8080
